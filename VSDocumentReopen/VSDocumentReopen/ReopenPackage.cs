183c99ec4099d2e96f73674405121f328b40523b
Imre Toth <imre@ascentsoftware.eu>
2018-07-06 17:21:31 +0200

    Enforce Ctrl+Shift+T key binding.

--- VSDocumentReopen/VSDocumentReopen/ReopenPackage.cs
+++ VSDocumentReopen/VSDocumentReopen/ReopenPackage.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,30 +19,12 @@ using Task = System.Threading.Tasks.Task;
 
 namespace VSDocumentReopen
 {
-	/// <summary>
-	/// This is the class that implements the package exposed by this assembly.
-	/// </summary>
-	/// <remarks>
-	/// <para>
-	/// The minimum requirement for a class to be considered a valid package for Visual Studio
-	/// is to implement the IVsPackage interface and register itself with the shell.
-	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-	/// to do it: it derives from the Package class that provides the implementation of the
-	/// IVsPackage interface and uses the registration attributes defined in the framework to
-	/// register itself and its components with the shell. These attributes tell the pkgdef creation
-	/// utility what data to put into .pkgdef file.
-	/// </para>
-	/// <para>
-	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-	/// </para>
-	/// </remarks>
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
 	[Guid(ReopenPackage.PackageGuidString)]
 	[ProvideAutoLoad(UIContextGuids.NoSolution)]
-	//[ProvideAutoLoad(UIContextGuids.SolutionExists)]
 	public sealed class ReopenPackage : AsyncPackage
 	{
 		/// <summary>
@@ -77,15 +60,6 @@ namespace VSDocumentReopen
 			_dte.ItemOperations.OpenFile(document.Name, document.Kind);
 		}
 
-		#region Package Members
-
-		/// <summary>
-		/// Initialization of the package; this method is called right after the package is sited, so this is the place
-		/// where you can put all the initialization code that rely on services provided by VisualStudio.
-		/// </summary>
-		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
-		/// <param name="progress">A provider for progress updates.</param>
-		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
 		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
 		{
 			// When initialized asynchronously, the current thread may be a background thread at this point.
@@ -93,30 +67,46 @@ namespace VSDocumentReopen
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 			await Reopen.InitializeAsync(this);
 
-			List<Command> commands = new List<Command>();
+			EnforceKeyBinding();
+		}
 
+		/// <summary>
+		/// Enforcing the Ctrl+Shift+T because if it was assigned to another command then the default does not work.
+		/// </summary>
+		private void EnforceKeyBinding()
+		{
+			var commands = new List<Command>();
 			foreach (Command command in _dte.Commands)
 			{
 				if (!string.IsNullOrEmpty(command.Name))
 					commands.Add(command);
 			}
 
-			foreach (var dteCommand in commands)
+			Command comm;
+			var guid = Reopen.CommandSet.ToString("B").ToUpper();
+			var binding = "Global::Ctrl+Shift+T";
+
+			foreach (var command in commands)
 			{
-				if (dteCommand.Bindings is object[] bindings && bindings.Length > 0)
+				if (command.Bindings is object[] bindings && bindings.Length > 0)
 				{
-					
+					var bind = string.Join(" ", bindings.Select(x => (string)x));
+					if (bind.Contains(binding) && command.Guid != guid)
+					{
+						command.Bindings = new object[0];
+					}
 				}
 			}
 
-			if (GetService(typeof(IMenuCommandService)) is OleMenuCommandService mcs)
+			foreach (var command in commands)
 			{
-				//CommandID menuCommandID = new CommandID(GuidList.guidShortcutExporterCmdSet, (int)PkgCmdIDList.cmdExportShortcuts);
-				//MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
-				//mcs.AddCommand(menuItem);
+				if (command.Guid == guid)
+				{
+					comm = command;
+					comm.Bindings = binding;
+					break;
+				}
 			}
 		}
-
-		#endregion
 	}
 }
