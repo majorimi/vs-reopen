85b2e34cc8b50ad3dca693b17848acb951176d75
Imre Toth <majorimi@hotmail.com>
2018-07-09 23:25:29 +0200

    Move command to a sub-menu add Clear history command.

--- VSDocumentReopen/VSDocumentReopen/ReopenPackage.cs
+++ VSDocumentReopen/VSDocumentReopen/ReopenPackage.cs
@@ -46,7 +46,9 @@ namespace VSDocumentReopen
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
 			await Reopen.InitializeAsync(this, _dte);
+			await ClearHistory.InitializeAsync(this);
 
 			EnforceKeyBinding();
 
